20d006b911851567f2a92a441a142e0ac4bf1db7
freitasxandrei <freitasxandrei@gmail.com>
2023-08-26 17:15:08 -0300

    firstCommit

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/wBnZvwsw/newteste.png',
+    title: "In progress",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/3J36d4dQ/newfocus.png',
+    title: "Focus",
+    desc: "",
+    link: "",
+    git: "https://github.com/freitasxandrei/focusPI",
+  },
+  
+];
